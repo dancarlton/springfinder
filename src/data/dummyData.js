fecb4503f095186b00eeee49f6b1786116a1fb60
Dan Carlton <danlouis.carlton@gmail.com>
2024-07-12 15:07:08 -0700

    added dummydata

---
+++ src/data/dummyData.js
@@ -0,0 +1,28 @@
+const dummyData = {
+    "springs": [
+      {
+        "name": "Travertine Hot Springs",
+        "location": {
+          "latitude": 38.2451,
+          "longitude": -119.2114
+        },
+        "description": "Located in Bridgeport, California with several pools.",
+        "temperature": "Varies",
+        "accessible": true
+      },
+      {
+        "name": "Buckeye Hot Springs",
+        "location": {
+          "latitude": 38.3202,
+          "longitude": -119.5233
+        },
+        "description": "Hot springs near a creek in Bridgeport, California.",
+        "temperature": "Varies",
+        "accessible": false
+      }
+      // Add more spring entries here
+    ]
+  }
+
+
+  export default dummyData
