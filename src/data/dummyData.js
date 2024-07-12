19ecb041a36d37a128eb476cfd70e9c07d4f45c7
Dan Carlton <danlouis.carlton@gmail.com>
2024-07-12 15:12:13 -0700

    5 pools added to dummyData

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -1,57 +1,123 @@
 const dummyData = {
-    "springs": [
-      {
-        "name": "Travertine Hot Springs",
-        "location": {
-          "latitude": 38.2451,
-          "longitude": -119.2114
-        },
-        "description": "Located in Bridgeport, California with several pools.",
-        "temperature": "Varies",
-        "accessible": true
+  springs: [
+    {
+      name: 'Travertine Hot Springs',
+      location: {
+        latitude: 38.2451,
+        longitude: -119.2114,
       },
-      {
-        "name": "Buckeye Hot Springs",
-        "location": {
-          "latitude": 38.3202,
-          "longitude": -119.5233
-        },
-        "description": "Hot springs near a creek in Bridgeport, California.",
-        "temperature": "Varies",
-        "accessible": false
+      description: 'Located in Bridgeport, California with several pools.',
+      temperature: 'Varies',
+      accessible: true,
+    },
+    {
+      name: 'Buckeye Hot Springs',
+      location: {
+        latitude: 38.3202,
+        longitude: -119.5233,
       },
-      {
-        name: "Wild Willy's Hot Springs",
-        location: {
-          latitude: 37.6474,
-          longitude: -118.8095,
-        },
-        description: "Popular hot springs in Mammoth Lakes, California.",
-        temperature: "102°F - 105°F",
-        accessible: true,
+      description: 'Hot springs near a creek in Bridgeport, California.',
+      temperature: 'Varies',
+      accessible: false,
+    },
+    {
+      name: "Wild Willy's Hot Springs",
+      location: {
+        latitude: 37.6474,
+        longitude: -118.8095,
       },
-      {
-        name: "Saline Valley Warm Springs",
-        location: {
-          latitude: 36.8061,
-          longitude: -117.7516,
-        },
-        description: "Remote hot springs located in Death Valley National Park.",
-        temperature: "Varies",
-        accessible: false,
+      description: 'Popular hot springs in Mammoth Lakes, California.',
+      temperature: '102°F - 105°F',
+      accessible: true,
+    },
+    {
+      name: 'Saline Valley Warm Springs',
+      location: {
+        latitude: 36.8061,
+        longitude: -117.7516,
       },
-      {
-        name: "Sykes Hot Springs",
-        location: {
-          latitude: 36.2345,
-          longitude: -121.7641,
-        },
-        description: "Natural hot springs located in Big Sur, California.",
-        temperature: "100°F - 105°F",
-        accessible: true,
+      description: 'Remote hot springs located in Death Valley National Park.',
+      temperature: 'Varies',
+      accessible: false,
+    },
+    {
+      name: 'Sykes Hot Springs',
+      location: {
+        latitude: 36.2345,
+        longitude: -121.7641,
       },
-    ]
-  }
+      description: 'Natural hot springs located in Big Sur, California.',
+      temperature: '100°F - 105°F',
+      accessible: true,
+    },
+  ],
+  pools: [
+    {
+      name: 'Red Hook Wading Pool',
+      location: {
+        borough: 'Brooklyn',
+        community_board: 306,
+        council_district: 38,
+        latitude: 40.673058856678175,
+        longitude: -74.00361401114736,
+      },
+      description: 'Outdoor wading pool located in Brooklyn.',
+      pool_type: 'Wading',
+      accessible: true,
+    },
+    {
+      name: 'Floating Pool',
+      location: {
+        borough: 'Bronx',
+        community_board: 202,
+        council_district: 17,
+        latitude: 40.804149906408306,
+        longitude: -73.8889859664428,
+      },
+      description: 'Outdoor intermediate pool located in the Bronx.',
+      pool_type: 'Intermediate',
+      accessible: true,
+    },
+    {
+      name: 'Bushwick Wading Pool',
+      location: {
+        borough: 'Brooklyn',
+        community_board: 301,
+        council_district: 34,
+        latitude: 40.70191944164836,
+        longitude: -73.93984138673747,
+      },
+      description: 'Outdoor wading pool located in Bushwick, Brooklyn.',
+      pool_type: 'Wading',
+      accessible: false,
+    },
+    {
+      name: 'Lasker Pool',
+      location: {
+        borough: 'Manhattan',
+        community_board: 164,
+        council_district: 6,
+        latitude: 40.7961714814837,
+        longitude: -73.95463274927891,
+      },
+      description: 'Outdoor Olympic pool located in Manhattan.',
+      pool_type: 'Olympic',
+      accessible: true,
+    },
+    {
+      name: 'Liberty Pool',
+      location: {
+        borough: 'Queens',
+        community_board: 412,
+        council_district: 27,
+        latitude: 40.70163161256614,
+        longitude: -73.78408764500973,
+      },
+      description: 'Outdoor intermediate pool located in Liberty, Queens.',
+      pool_type: 'Intermediate',
+      accessible: true,
+    },
+  ],
+}
 
-
-  export default dummyData
+export default dummyData
