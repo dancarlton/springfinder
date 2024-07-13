aca60fa59e0d30f90140f52d2f1e625910116b07
Alpha Bah <alphabah2395@gmail.com>
2024-07-13 14:25:38 -0400

    carousel feature

--- src/components/common/Cards.jsx
+++ src/components/common/Cards.jsx
@@ -1,45 +1,11 @@
-import { useEffect, useState } from 'react'
-
-import { dummyData } from '../../data/dummyData'
-
-const Cards = () => {
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    const processedData = []
-
-    // extract data from springs
-    dummyData.springs.forEach(card => {
-      processedData.push({
-        name: card.name,
-        image: card.image,
-        description: card.description,
-      })
-    })
-
-    // extract data from pools
-    dummyData.pools.forEach(card => {
-      processedData.push({
-        name: card.name,
-        image: card.image,
-        description: card.description,
-      })
-    })
-
-    setData(processedData)
-  }, [])
-
+const Cards = ({ item }) => {
   return (
-    <div className='card__container'>
-      {data.map((item, index) => (
-        <div key={index} className='card'>
-          <img src={item.image} alt={item.name} className='card__image' />
-          <h3 className='card__name'>{item.name}</h3>
-          <p className='card__description'>{item.description}</p>
-        </div>
-      ))}
+    <div className='card'>
+      <img src={item.image} alt={item.name} className='card__image' />
+      <h3 className='card__name'>{item.name}</h3>
+      <p className='card__description'>{item.description}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
